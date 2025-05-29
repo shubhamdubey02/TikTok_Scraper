f855f3b6dd557e2a0ba35672ac3608a7fd129d59
Shubham dubey <shubham.dubey@oodles.io>
2025-05-29 11:34:58 +0530

    scrole

--- src/scrapers/parseCategories.js
+++ src/scrapers/parseCategories.js
@@ -1,22 +1,6 @@
 const puppeteer = require('puppeteer');
+const { autoScroll } = require('../helpers/AutoScrollCategories');
 
-async function autoScroll(page) {
-    await page.evaluate(async () => {
-        await new Promise((resolve) => {
-            let totalHeight = 0;
-            const distance = 100;
-            const timer = setInterval(() => {
-                const scrollHeight = document.body.scrollHeight;
-                window.scrollBy(0, distance);
-                totalHeight += distance;
-                if (totalHeight >= scrollHeight) {
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, 100);
-        });
-    });
-}
 
 (async () => {
     const browser = await puppeteer.launch({
